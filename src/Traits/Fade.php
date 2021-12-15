ee3b716a24f61b8f5864c23a5e138b52918c5ba1
Christian Clark <cclark61@gmail.com>
2021-12-14 19:01:25 -0500

    Fill / Fade method development.

--- src/Traits/Fade.php
+++ src/Traits/Fade.php
@@ -2,9 +2,10 @@
 //*****************************************************************************
 //*****************************************************************************
 /**
- * WS2812 Node Class
+ * WS2812 Node Fade Trait
  *
  * @package         Cclark61\RPi\WS2812
+ * @subpackage      Traits
  * @author          Christian J. Clark
  * @copyright       Christian J. Clark
  * @link            https://github.com/cclark61/php-rpi-ws2812
@@ -12,14 +13,25 @@
 //*****************************************************************************
 //*****************************************************************************
 
-namespace Cclark61\RPi\WS2812;
+namespace Cclark61\RPi\WS2812\Traits;
 
-class Node
+trait Fade
 {
     //=========================================================================
-    // Traits
     //=========================================================================
-    use Traits\Core;
-    use Traits\Fill;
-    use Traits\Fade;
+    // Fade Method
+    //=========================================================================
+    //=========================================================================
+    public function Fade(Array $args=[])
+    {
+        $start_brightness = 0;
+        $end_brightness = 255;
+        $delay = 50;
+        $step = 1;
+        $args = $this->DefaultCommandArgs($args);
+        extract($args);
+        $cmd = "fade {$start_brightness}, {$end_brightness}, {$delay}, {$start}, {$len}";
+        $this->WriteCommand($cmd);
+    }
+
 }
